114dff09cee8f9d8f9f4c3f20b6aa9dfaba39434
Martin <166133291+MH020@users.noreply.github.com>
2025-12-23 17:57:02 +0100

    bug ?

--- chmod-parser/src/main.rs
+++ chmod-parser/src/main.rs
@@ -1,5 +1,11 @@
-mod chmod;
 mod perms;
+use perms::NyPermsT;
 fn main() {
 
+    let mut file_a = NyPermsT {bits: 0o644};
+    println!("test 1 before: {}", file_a.bits);
+    let mut expect = NyPermsT {bits: 0o640};
+    println!("test 1 expect: {}", expect.bits);
+    file_a.chmod("o-r".to_string()); 
+    println!("test 1 actual: {}", file_a.bits);
 }
