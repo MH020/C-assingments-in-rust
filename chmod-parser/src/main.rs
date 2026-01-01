8ba80b57a9a1293d48713a7b91e1f873088fea6d
Martin <166133291+MH020@users.noreply.github.com>
2026-01-01 10:47:47 +0000

    works with octals?

--- chmod-parser/src/main.rs
+++ chmod-parser/src/main.rs
@@ -2,9 +2,9 @@ mod perms;
 use perms::NyPermsT;
 fn main() {
 
-    let mut file_a = NyPermsT {bits: 0o644};
+    let mut file_a = NyPermsT {bits: 0644};
     println!("test 1 before: {}", file_a.bits);
-    let mut expect = NyPermsT {bits: 0o640};
+    let mut expect = NyPermsT {bits: 0640};
     println!("test 1 expect: {}", expect.bits);
     file_a.chmod("o-r".to_string()); 
     println!("test 1 actual: {}", file_a.bits);
