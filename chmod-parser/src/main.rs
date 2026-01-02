c50bccd79e14d1a98b66c38905b4a44a01bc2c8b
Martin <166133291+MH020@users.noreply.github.com>
2026-01-02 16:15:25 +0100

    Update main.rs

--- chmod-parser/src/main.rs
+++ chmod-parser/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
     let mut file_a = NyPermsT {bits: 0644};
     println!("test 1 before: {}", file_a.bits);
-    let mut expect = NyPermsT {bits: 0640};
+    let expect = NyPermsT {bits: 0640};
     println!("test 1 expect: {}", expect.bits);
     file_a.chmod("o-r".to_string()); 
     println!("test 1 actual: {}", file_a.bits);
